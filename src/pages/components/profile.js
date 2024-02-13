8a03d4948106f9012d5e3a444bd075fd1ef0b50d
Yongxi-Zhou <chow971@outlook.com>
2024-02-13 12:05:36 +0800

    update todo list

--- src/pages/components/profile.js
+++ src/pages/components/profile.js
@@ -1,6 +1,13 @@
+import TodoList from "../qcomps/todos";
+
 export default function Profile() {
-  return (<img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />)
+  return (
+  <>
+    <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+      <TodoList />
+  </>
+    )
 }
