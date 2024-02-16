70a4e3532f40b155c6bd3ebe5cea04a8ce103a1e
Yongxi-Zhou <chow971@outlook.com>
2024-02-16 23:39:29 +0800

    update activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+// import List from './components/list_keys'
+import List from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
+import Button from './components/button'
+import AButton from './qcomps/menuBar'
+import Gallery from './qcomps/state'
+import PersonData from './components/stateObj'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/* <Profile /> */}
+        <PersonData/>
     </div>
   )
 }
