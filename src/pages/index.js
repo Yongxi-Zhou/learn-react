5e89470684b1f2cc39295ddc699ade40c3a5b2a2
Yongxi-Zhou <chow971@outlook.com>
2024-02-16 23:47:30 +0800

    update activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,19 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
-// import List from './components/list_keys'
-import List from './qcomps/list_keys_id'
-import RecipeList from './qcomps/recipes'
-import Button from './components/button'
-import AButton from './qcomps/menuBar'
-import Gallery from './qcomps/state'
-import PersonData from './components/stateObj'
+// import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import Form from './components/snapshot2'
+import Counter from './components/queueUpdates'
+import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         {/* <Profile /> */}
-        <PersonData/>
+        {/* <PersonData/> */}
+        <Scoreboard />
     </div>
   )
 }
